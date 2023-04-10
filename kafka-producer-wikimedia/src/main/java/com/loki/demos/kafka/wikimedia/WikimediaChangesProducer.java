d1f8e99e8cb9f8b282c3e34c2f36ff0aec49b2ef
armagansahin1 <75439200+armagansahin1@users.noreply.github.com>
2023-04-10 15:54:42 +0300

    high throughput producer configured

--- kafka-producer-wikimedia/src/main/java/com/loki/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/loki/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -3,6 +3,7 @@ package com.loki.demos.kafka.wikimedia;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
@@ -14,13 +15,17 @@ public class WikimediaChangesProducer {
     public static void main(String[] args) throws InterruptedException {
 
         String bootstrapServer = "127.0.0.1:9092";
-        //Create Producer Properties
 
+        //Create Producer Properties
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers",bootstrapServer);
-
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer",StringSerializer.class.getName());
+
+        // set high throughput producer configs
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG,"snappy");
 
         // Create the Producer
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
